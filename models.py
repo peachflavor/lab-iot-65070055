d9563e20cf550c3f90669052a0fcda177e8d2c76
peachflavor <134000454+peachflavor@users.noreply.github.com>
2024-07-17 21:57:45 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, Date, Integer, String
 # from sqlalchemy.orm import relationship
 
 from database import Base
 
-class Book(Base):
-    __tablename__ = 'books'
+class StudentInformation(Base):
+    __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    author = Column(String, index=True)
-    year = Column(Integer, index=True)
-    is_published = Column(Boolean, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    student_id = Column(String, unique=True, index=True)
+    birth_date = Column(Date)
+    gender = Column(String)
 
