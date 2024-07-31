e36924f56a7d0a9bb35975ce4bd759b067123d46
peachflavor <134000454+peachflavor@users.noreply.github.com>
2024-07-31 20:42:08 +0700

    Update models.py

--- models.py
+++ models.py
@@ -10,13 +10,35 @@ class Book(Base):
     title = Column(String, index=True)
     author = Column(String, index=True)
     year = Column(Integer, index=True)
+    detail = Column(String, index=True)
+    story = Column(String, index=True)
+    classification = Column(String, index=True)
     is_published = Column(Boolean, index=True)
 
+class Menu(Base):
+    __tablename__ = 'menus'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer, index=True)
+    detail = Column(String, index=True)
+    ingredient = Column(String, index=True)
+    is_published = Column(Boolean, index=True)
+    
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer, index=True)
+    total = Column(Integer, index=True)
+    note = Column(String, index=True)
+    
 class Student(Base):
     __tablename__ = 'students'
 
-    firstname = Column(String, index=True)
-    lastname = Column(String, index=True)
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    lastname = Column(String, index=True)
     dob = Column(String, index=True)
-    gender = Column(String, index=True)
+    sex = Column(String, index=True)
